cb3d3c9065b28e3f8b2887e65cff18524f184c1b
Kedar Kolhatkar <kedarkol@amazon.com>
2021-08-08 12:19:20 -0400

    added pre-push hook

--- test/node-starter.test.ts
+++ test/node-starter.test.ts
@@ -1,18 +1,20 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as LambdaStack from '../lib/lambda-stack';
+import { APIGatewayStack, APIGatewayStackProps } from '../lib/api-stack';
 
-test.skip('Empty Stack', () => {
+test('APIGatewayStack', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new LambdaStack(app, 'MyTestStack');
+  const props = {
+    description: 'The API stack for user-service. This is a root level stack for the user microservice.',
+    restApiName: 'userServiceAPI',
+    apiDescription: 'This service provides functionality to manager users',
+    srcDirectory: '../build',
+    lambdaFunctionName: 'user-service-handler',
+    terminationProtection: true,
+  } as APIGatewayStackProps;
+
+  const stack = new APIGatewayStack(app, 'MyTestStack', props);
   // THEN
-  expectCDK(stack).to(
-    matchTemplate(
-      {
-        Resources: {},
-      },
-      MatchStyle.EXACT,
-    ),
-  );
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
