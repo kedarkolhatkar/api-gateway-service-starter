9e526e6d16cea2f4b00e15f7dde55b78326a4ed6
Kedar Kolhatkar <kedarkol@amazon.com>
2021-08-09 00:21:41 -0400

    removed extension from import to correct deployment failure

--- bin/service.ts
+++ bin/service.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { APIGatewayStack, APIGatewayStackProps } from '../lib/api-stack.js';
+import { APIGatewayStack, APIGatewayStackProps } from '../lib/api-stack';
 
 const app = new cdk.App();
 
