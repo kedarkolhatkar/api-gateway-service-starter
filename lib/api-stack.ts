cf648b086c07883dafddda8aaec3b34c5cea806c
Kedar Kolhatkar <kedarkol@amazon.com>
2021-08-09 23:50:32 -0400

    added User dynamodb table

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as path from 'path';
 import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import { TableEncryption } from '@aws-cdk/aws-dynamodb';
 
 /**
  *
@@ -17,8 +19,16 @@ export class APIGatewayStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: APIGatewayStackProps) {
     super(scope, id, props);
 
+    const userTableName = 'user-table';
+    const userTable = new dynamodb.Table(this, `${userTableName}-id`, {
+      tableName: userTableName,
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      encryption: TableEncryption.AWS_MANAGED,
+    });
+
     const lambdaFunctionSrcDir = `${props.srcDirectory}/${props.lambdaFunctionName}`;
-    console.log(path.join(__dirname, lambdaFunctionSrcDir));
+    // console.log(path.join(__dirname, lambdaFunctionSrcDir));
 
     // Define the backend lambda
     const backend = new lambda.Function(this, `${props.lambdaFunctionName}-id`, {
@@ -26,6 +36,10 @@ export class APIGatewayStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: `${props.lambdaFunctionName}.handler`,
       code: lambda.Code.fromAsset(path.join(__dirname, lambdaFunctionSrcDir)),
+      environment: {
+        USER_TABLE_ARN: userTable.tableArn,
+        USER_TABLE_NAME: userTable.tableName,
+      },
     });
 
     // Create User API
