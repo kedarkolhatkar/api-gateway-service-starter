9ac501a7ce11b4b167a328028058916a3b540130
Kedar Kolhatkar <kedarkol@amazon.com>
2021-08-16 22:50:39 -0400

    Added PipelineStackProps

--- pipeline/lib/pipeline-stack.ts
+++ pipeline/lib/pipeline-stack.ts
@@ -4,14 +4,27 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipelineActions from '@aws-cdk/aws-codepipeline-actions';
 import { LogGroup } from '@aws-cdk/aws-logs';
 
+export interface PipelineStackProps extends cdk.StackProps {
+  projectName: string;
+  serviceName: string;
+  buildspecsDir?: string;
+  repoName: string;
+  repoOwner: string;
+  repoAccessTokenName: string;
+  branch?: string;
+}
+
 export class PipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
 
+    const defaultProps = { buildSpecsDir: 'pipeline/buildspecs', branch: 'main' };
+    const finalProps = { ...defaultProps, ...props };
+
     // First build the code build project
-    const cdkBuild = new codebuild.PipelineProject(this, 'user-service-build-1', {
-      projectName: 'user-service-build-project',
-      description: 'This code build project, compiles, unit tests and deploys to development environment',
+    const cdkBuild = new codebuild.PipelineProject(this, `${finalProps.serviceName}-build-1`, {
+      projectName: `${finalProps.serviceName}-build-project`,
+      description: `This code build project for ${finalProps.serviceName} compiles, unit tests and deploys to development environment`,
       buildSpec: codebuild.BuildSpec.fromSourceFilename('pipeline/buildspecs/dev.yaml'),
       concurrentBuildLimit: 1,
       environment: {
@@ -20,19 +33,18 @@ export class PipelineStack extends cdk.Stack {
       logging: {
         cloudWatch: {
           enabled: true,
-          logGroup: LogGroup.fromLogGroupName(this, 'user-service-buildlogs-id', 'hamlet-buildlogs'),
-          prefix: 'user-service-buildlogs',
+          logGroup: LogGroup.fromLogGroupName(this, `${finalProps.serviceName}-buildlogs-id`, finalProps.projectName),
+          prefix: `${finalProps.serviceName}-buildlogs`,
         },
       },
     });
 
     const sourceOutput = new codepipeline.Artifact('SrcOutput');
-    const githubToken = cdk.SecretValue.secretsManager('hamlet-github-token');
-    console.log(`github token: ${githubToken}`);
+    const githubToken = cdk.SecretValue.secretsManager(finalProps.repoAccessTokenName);
 
     // Create code pipeline
     // TODO: Use SecretManager to get token
-    new codepipeline.Pipeline(this, 'user-service-pipeline', {
+    new codepipeline.Pipeline(this, `${finalProps.serviceName}-pipeline`, {
       restartExecutionOnUpdate: true,
       stages: [
         {
@@ -41,9 +53,9 @@ export class PipelineStack extends cdk.Stack {
             new codepipelineActions.GitHubSourceAction({
               actionName: 'Checkout',
               output: sourceOutput,
-              repo: 'hamlet-user-service',
-              branch: 'main',
-              owner: 'kedarkolhatkar',
+              repo: finalProps.repoName,
+              branch: finalProps.branch,
+              owner: finalProps.repoOwner,
               oauthToken: githubToken,
             }),
           ],
